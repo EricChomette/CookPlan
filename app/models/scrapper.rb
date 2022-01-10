2e9ed017b37b418b1156c66e4c414c3404ff1bb7
EricChomette <eric.chomette@gmail.com>
2022-01-10 16:00:13 +0100

    add gemfile

--- app/models/scrapper.rb
+++ app/models/scrapper.rb
@@ -1,34 +1,37 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+require 'csv'
 
 class Scraper
 
   def recipes
-    # [img_recipe, recipe_name , category, resum  ]
+    recipe_array = [img, name, category, resume]
 
     base_url = "https://www.lesrecettesdecuisine.com/"
     main_url = "#{base_url}recette-de-cuisine/category/les-desserts"
     # The open method take the url, and return and html file
     data_recipes = data_scraper(main_url)
 
-    # # image lien
-    # p "Image de la recette"
-    # recipes_content = data_recipes.css('.post-thumbnail > a img').each do |object|
-    #   p object.attr('data-src')
-    # end
-
-    # p "Titre des recette"
-    # p recipes_content = data_recipes.css('.post-entry-content > h3 > a').text
-    # # category
-    p "Catergory de la recette"
-    recipes_content_category = data_recipes.css('.post-entry-content > .entry-meta > .category-link > a').each do |t|
-      p t.text if t.text != "Vos Recettes"
+    p "URL DES IMAGES"
+    url_img = data_recipes.css('.post-thumbnail > a img').each do |object|
+      p object.attr('data-src')
     end
 
-    # résumé de la recette
-    # p "Résumé de la recette"
-    # p recipes_content_category = data_recipes.css('.entry-excerpt > p').text
+    p "NOMS DES RECETTES"
+    recipe_name = data_recipes.css('.post-entry-content > h3 > a').each do |t|
+      p "- #{t.text}"
+    end
+
+    p "CATEGORIES"
+    recipes_content_category = data_recipes.css('.post-entry-content > .entry-meta > .category-link > a').each do |t|
+      p "- #{t.text}" if t.text != "Vos Recettes"
+    end
+
+    p "RESUME DES RECETTES"
+    recipes_content_category = data_recipes.css('.entry-excerpt > p').each do |t|
+      p "- #{t.text}"
+    end
   end
 
   def data_scraper(url)
@@ -40,6 +43,3 @@ end
 
 scrape = Scraper.new
 scrape.recipes
-
-# class selector image -> post-thumbnail
-# class selector text -> post-entry-content
