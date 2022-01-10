70b14a7f9b24100b4b19f3159e478044169cca96
EricChomette <eric.chomette@gmail.com>
2022-01-10 13:49:10 +0100

    save

--- app/models/scrapper.rb
+++ app/models/scrapper.rb
@@ -5,13 +5,25 @@ require 'pry'
 class Scraper
 
   def recipes
+    # [img_recipe, recipe_name , category, resum  ]
+
     base_url = "https://www.lesrecettesdecuisine.com/"
     main_url = "#{base_url}recette-de-cuisine/category/les-desserts"
     # The open method take the url, and return and html file
     data_recipes = data_scraper(main_url)
 
-    p recipes_img = data_recipes.css('.post-thumbnail')
-    p recipes_content = data_recipes.css('.post-entry-content')
+    # image
+    p "Image de la recette"
+    p recipes_content = data_recipes.css('.post-thumbnail > a > .lazy-load')["src"]
+    # titre
+    p "Titre des recette"
+    p recipes_content = data_recipes.css('.post-entry-content > h3 > a').text
+    # category
+    p "Catergory de la recette"
+    p recipes_content_category = data_recipes.css('.post-entry-content > .entry-meta > .category-link > a').text
+    p "Résumé de la recette"
+    p recipes_content_category = data_recipes.css('.entry-excerpt > p').text
+
   end
 
   def data_scraper(url)
@@ -21,8 +33,8 @@ class Scraper
 
 end
 
-# class selector image -> post-thumbnail
-# class selector text -> post-entry-content
-
 scrape = Scraper.new
 scrape.recipes
+
+# class selector image -> post-thumbnail
+# class selector text -> post-entry-content
