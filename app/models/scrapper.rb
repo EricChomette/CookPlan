fb0beb9f834475bfb898e870d5f9120f7a80e60e
EricChomette <eric.chomette@gmail.com>
2022-01-10 15:33:58 +0100

    scrapping catégory

--- app/models/scrapper.rb
+++ app/models/scrapper.rb
@@ -12,24 +12,23 @@ class Scraper
     # The open method take the url, and return and html file
     data_recipes = data_scraper(main_url)
 
-    # image lien
-    p "Image de la recette"
-    recipes_content = data_recipes.css('.post-thumbnail > a img').each do |object|
-      p object.attr('data-src')
+    # # image lien
+    # p "Image de la recette"
+    # recipes_content = data_recipes.css('.post-thumbnail > a img').each do |object|
+    #   p object.attr('data-src')
+    # end
+
+    # p "Titre des recette"
+    # p recipes_content = data_recipes.css('.post-entry-content > h3 > a').text
+    # # category
+    p "Catergory de la recette"
+    recipes_content_category = data_recipes.css('.post-entry-content > .entry-meta > .category-link > a').each do |t|
+      p t.text if t.text != "Vos Recettes"
     end
 
-    p "Titre des recette"
-    p recipes_content = data_recipes.css('.post-entry-content > h3 > a').text
-    # category
-    p "Catergory de la recette"
-    p recipes_content_category = data_recipes.css('.post-entry-content > .entry-meta > .category-link > a').text
-    #  .each do |line|
-      line.text
-    end
     # résumé de la recette
     # p "Résumé de la recette"
     # p recipes_content_category = data_recipes.css('.entry-excerpt > p').text
-
   end
 
   def data_scraper(url)
