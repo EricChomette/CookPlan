271e55eb8c4e6d316fccb44c83e28137d60434e0
EricChomette <eric.chomette@gmail.com>
2022-01-10 12:32:48 +0100

    save

--- app/models/scrapper.rb
+++ app/models/scrapper.rb
@@ -6,22 +6,21 @@ class Scraper
 
   def recipes
     base_url = "https://www.lesrecettesdecuisine.com/"
-    main_url = "https://www.lesrecettesdecuisine.com/recette-de-cuisine/category/les-desserts"
+    main_url = "#{base_url}recette-de-cuisine/category/les-desserts"
     # The open method take the url, and return and html file
-    data = data_scraper(main_url)
-    recipes = doc.css('.articles-list').css('.post-entry-content')
+    data_recipes = data_scraper(main_url)
+    recipes = data_recipes.css('.post-thumbnail').css('.post-entry-content')
     p recipes
-    binding.pry
     end
 
-    def data_scraper(url)
-      html = URI.open(url)
-      doc  = Nokogiri::HTML(html)
-    end
+  def data_scraper(url)
+    html = URI.open(url)
+    doc  = Nokogiri::HTML(html)
+  end
 end
 
 # class selector image -> post-thumbnail
 # class selector text -> post-entry-content
 
-scrape = Scraper.new
-scrape.scrape_recipe_url
+# scrape = Scraper.new
+# scrape.scrape_recipe_url
