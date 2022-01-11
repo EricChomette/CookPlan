5fce6be15a5bc545761cdaa36b03b38250d4860c
EricChomette <eric.chomette@gmail.com>
2022-01-11 10:09:20 +0100

    scrapper

--- app/models/scrapper.rb
+++ app/models/scrapper.rb
@@ -6,40 +6,83 @@ require 'csv'
 class Scraper
 
   def recipes
-    recipe_array = [img, name, category, resume]
-
-    base_url = "https://www.lesrecettesdecuisine.com/"
+    base_url = 'https://www.lesrecettesdecuisine.com/'
     main_url = "#{base_url}recette-de-cuisine/category/les-desserts"
-    # The open method take the url, and return and html file
     data_recipes = data_scraper(main_url)
+    recipe_array = []
+    array_url_img = []
+    array_name = []
+    array_category = []
+    array_resum = []
 
-    p "URL DES IMAGES"
-    url_img = data_recipes.css('.post-thumbnail > a img').each do |object|
-      p object.attr('data-src')
+    p 'NOMS DES RECETTES'
+    data_recipes.css('.post .post-entry-content > h3 > a').each do |t|
+      array_name << t.text
     end
+    p nbr_target = array_name.count
 
-    p "NOMS DES RECETTES"
-    recipe_name = data_recipes.css('.post-entry-content > h3 > a').each do |t|
-      p "- #{t.text}"
+    p 'URL DES IMAGES'
+    data_recipes.css('.post .post-thumbnail > a img').each do |object|
+      if object.attr('data-src') == nil && data_recipes.css('.post .post-entry-content > h3 > a').exist
+        array_url_img << 0
+      else
+        array_url_img << object.attr('data-src')
+      end
     end
+    p array_url_img.count
 
-    p "CATEGORIES"
-    recipes_content_category = data_recipes.css('.post-entry-content > .entry-meta > .category-link > a').each do |t|
-      p "- #{t.text}" if t.text != "Vos Recettes"
+    p 'CATEGORIES'
+    data_recipes.css('.post-entry-content > .entry-meta > .category-link > a').each do |t|
+      array_category << t.text if t.text != 'Vos Recettes'
     end
+    p array_category.count
 
-    p "RESUME DES RECETTES"
-    recipes_content_category = data_recipes.css('.entry-excerpt > p').each do |t|
-      p "- #{t.text}"
+    p 'RESUME DES RECETTES'
+    data_recipes.css('.entry-excerpt > p').each do |t|
+      array_resum << t.text
     end
+    p array_resum.count
+
+    # array_name.each_with_index do |t, index|
+    #   recipe_array << [array_name[index], array_category[index], array_resum[index], array_url_img[index]]
+    # end
+
+    p recipe_array
   end
 
   def data_scraper(url)
     html = URI.open(url)
     doc  = Nokogiri::HTML(html)
   end
-
 end
 
 scrape = Scraper.new
 scrape.recipes
+
+
+    # CSV.foreach("/Users/ericchomette/code/revision/Cooklist/CookPlan/db/recipe.csv") do |row|
+    # end
+
+    # p "NOMS DES RECETTES"
+    # recipe_name = data_recipes.css('.post-entry-content > h3 > a').each do |t|
+    #  array_name << t.text
+    # end
+    # p array_name.count
+
+    # p "URL DES IMAGES"
+    # url_img = data_recipes.css('.post-thumbnail > a img').each do |object|
+    #   array_url_img << object.attr('data-src')
+    # end
+    # p array_url_img.count
+
+    # p "CATEGORIES"
+    # recipes_content_category = data_recipes.css('.post-entry-content > .entry-meta > .category-link > a').each do |t|
+    #   array_category << t.text if t.text != "Vos Recettes"
+    # end
+    # p array_category.count
+
+    # p "RESUME DES RECETTES"
+    # recipes_content_category = data_recipes.css('.entry-excerpt > p').each do |t|
+    #   array_resum << t.text
+    # end
+    # p array_resum.count
