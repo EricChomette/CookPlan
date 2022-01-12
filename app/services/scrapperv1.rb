cf537aa61693cc5a8ceaf3c81811f8c6b62d263f
EricChomette <eric.chomette@gmail.com>
2022-01-12 15:43:26 +0100

    save

--- app/services/scrapperv1.rb
+++ app/services/scrapperv1.rb
@@ -9,10 +9,16 @@ class Scraper
     url = "https://www.lesrecettesdecuisine.com/liste-recettes-cuisine-html"
     html = URI.open(url)
     doc  = Nokogiri::HTML(html)
-    recipes = doc.css('.post').css('.entry-title')
-    binding.pry
+    recipes = doc.css('.post').css('.entry-title').css('a')
+
+    recipes.each do |recipe|
+      url_recipe = recipe.attribute('href').value
+      binding.pry
+    end
+
   end
 
 end
-  scrape = Scraper.new
-  scrape.scrape_city_urls
+
+scrape = Scraper.new
+scrape.scrape_recipe_urls
